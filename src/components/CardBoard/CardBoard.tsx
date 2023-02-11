596c6decbbdd02ef932786785c2d2c45d4bcff42
brunobwn <brunoberwian@gmail.com>
2023-02-11 15:30:40 -0300

    change scroll lib to 3.x.x

--- src/components/CardBoard/CardBoard.tsx
+++ src/components/CardBoard/CardBoard.tsx
@@ -24,7 +24,7 @@ function CardBoard() {
 	}, []);
 
 	return (
-		<ScrollContainer className="fullscreen grabbable" innerRef={scrollContainer}>
+		<ScrollContainer className="fullscreen grabbable" ref={scrollContainer} hideScrollbars={true}>
 			<BigContainer>
 				{caracters.map((char, i) => {
 					const [x, y] = getRandomPosition();
