06cea1c0e6778cfbde366a2405e432960efcb0f1
brunobwn <brunoberwian@gmail.com>
2023-02-11 18:20:37 -0300

    add animations

--- src/components/SearchInput/SearchInput.tsx
+++ src/components/SearchInput/SearchInput.tsx
@@ -1,14 +1,102 @@
+import { AnimatePresence } from 'framer-motion';
+import { useEffect, useState, ChangeEvent } from 'react';
+import useDebounce from '../../hooks/useDebounce';
 import { BigInput, DropDownContainer, DropDownItem, InputContainer } from './styles';
-const chars = ['Fulano', 'Ciclano', 'Joano', 'Joano'];
+const chars = [
+	'Fulano1',
+	'Fulano142',
+	'Ciclano1',
+	'Joano1',
+	'Joana1',
+	'Ciclano2',
+	'Joano3',
+	'Joana3',
+	'Ciclano4',
+	'Joano44',
+	'Joana55',
+	'Ciclano67',
+	'Joano123',
+	'Joano56223',
+];
+
+const inputContainer = {
+	visible: { opacity: 1 },
+	hidden: { opacity: 0 },
+};
+
+const list = {
+	visible: { opacity: 1, y: 0 },
+	hidden: { opacity: 0, y: -100 },
+};
+
+const item = {
+	visible: { opacity: 1 },
+	hidden: { opacity: 0 },
+};
+
 function SearchInput() {
+	const [filterInput, setFilterInput] = useState('');
+	const [selected, setSelected] = useState(false);
+
+	const [filteredData, setFilteredData] = useState<string[] | null>(null);
+	const filterInputDebounce = useDebounce(filterInput, 700);
+
+	useEffect(() => {
+		handleFilter();
+	}, [filterInputDebounce]);
+
+	function handleFilter() {
+		if (!filterInput) {
+			setFilteredData(null);
+			return;
+		}
+		const newfilteredData = chars.filter((nome) =>
+			String(nome.toLowerCase()).includes(String(filterInput.toLowerCase()))
+		);
+
+		if (newfilteredData.length > 0) {
+			setFilteredData(newfilteredData);
+			return;
+		}
+
+		setFilteredData(null);
+	}
+
+	function handleCharacterClick(nome: string) {
+		setFilteredData(null);
+		setFilterInput(nome);
+		setSelected(true);
+	}
+
+	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+		setFilterInput(event.target.value);
+		if (selected) setSelected(false);
+	};
+
 	return (
-		<InputContainer>
-			<BigInput type="text" placeholder="Encontre seu personagem nos 7 reinos de Westeros" />
-			<DropDownContainer>
-				{chars.map((nome, i) => (
-					<DropDownItem key={i}>{nome}</DropDownItem>
-				))}
-			</DropDownContainer>
+		<InputContainer
+			variants={inputContainer}
+			initial="hidden"
+			animate="visible"
+			transition={{ delay: 1.5 }}
+		>
+			<BigInput
+				type="text"
+				placeholder="Encontre seu personagem nos 7 reinos de Westeros"
+				value={filterInput}
+				onChange={handleChange}
+			/>
+			{filteredData && !selected && (
+				<AnimatePresence>
+					<DropDownContainer variants={list} initial="hidden" animate="visible">
+						{filteredData.map((nome, i) => (
+							<DropDownItem key={i} onClick={() => handleCharacterClick(nome)} variants={item}>
+								{nome}
+							</DropDownItem>
+						))}
+					</DropDownContainer>
+				</AnimatePresence>
+			)}
 		</InputContainer>
 	);
 }
--- src/components/SearchInput/styles.tsx
+++ src/components/SearchInput/styles.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
-export const InputContainer = styled.div`
+export const InputContainer = styled(motion.div)`
 	width: 100%;
 	max-width: 45rem;
 	position: absolute;
@@ -25,27 +26,64 @@ export const BigInput = styled.input`
 	border-radius: 1rem;
 	color: #222;
 	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
-	border: 2px solid rgba(0, 0, 0, 0.8);
+	border: 1px solid rgba(212, 212, 212, 0.925);
 `;
 
-export const DropDownContainer = styled.ul`
+export const DropDownContainer = styled(motion.ul)`
 	width: 100%;
-	border-radius: 1rem;
+	border-radius: 0.2rem;
 	font-size: 1.2rem;
 	background-color: white;
-	border-radius: 1rem;
-	color: #222;
+	color: #424242;
 	margin-top: 0.5rem;
-	border: 1px solid rgba(0, 0, 0, 0.2);
+	border: 1px solid rgba(212, 212, 212, 0.925);
 	box-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
-	max-height: 13rem;
+	max-height: 12rem;
 	overflow: auto;
+
+	scrollbar-width: thin;
+	scrollbar-color: #cfcfcf96 #dfe9eb;
+	scrollbar-gutter: stable;
+	scroll-snap-type: y mandatory;
+	scroll-snap-align: start;
+
+	&::-webkit-scrollbar {
+		width: 5px;
+	}
+	&::-webkit-scrollbar-track {
+		border-radius: 20px;
+		background-color: #fff;
+		border: 20px solid transparent;
+	}
+
+	&::-webkit-scrollbar-track:hover {
+		background-color: #fff;
+	}
+
+	&::-webkit-scrollbar-track:active {
+		background-color: #fff;
+	}
+
+	&::-webkit-scrollbar-thumb {
+		border-radius: 5px;
+		background-color: #979797;
+		border: 2px solid transparent;
+	}
+
+	&::-webkit-scrollbar-thumb:hover {
+		background-color: #8c8c8c;
+	}
+
+	&::-webkit-scrollbar-thumb:active {
+		background-color: #515151;
+	}
 `;
 
-export const DropDownItem = styled.li`
+export const DropDownItem = styled(motion.li)`
 	width: 100%;
 	padding: 1rem;
 	box-shadow: 0px 1px 2px rgba(0, 0, 0, 0.05);
+	scroll-snap-align: start none;
 	&:hover {
 		background-color: rgba(0, 0, 0, 0.1);
 		cursor: pointer;
