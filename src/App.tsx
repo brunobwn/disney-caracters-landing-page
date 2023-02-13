cde17f39b96d820780f6fb0dfa2fc1249de575de
MarsKO <marcosilhaag31@gmail.com>
2023-02-12 22:55:20 -0300

    function getHouseSymbolURL

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import CardBoard from './components/CardBoard/CardBoard';
 import CharacterModal from './components/CharacterModal/CharacterModal';
 import { getCharacters } from './store/models/characterSlice';
-import { useAppDispatch } from './store/store';
+import { RootState, useAppDispatch } from './store/store';
 
 function App() {
 	const dispatch = useAppDispatch();
